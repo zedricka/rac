b81b91f5e5adae22fe4e6f31092d23e3870d4b68
Jomari Jacinto <jomarijacinto45@gmail.com>
2018-04-04 10:59:54 -1000

    Update dom.js

--- client/dom.js
+++ client/dom.js
@@ -5,3 +5,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//Testing adding to cart button with test button
+$(".cart").append("<button class='btn1'>Test</button>");
+//.click() so when button is clicked, it adds an HTML element to the front end
+$("button").click(function () {
+//meant to append requested HTML element to cart
+  $("#cartitem").append("<div>Test</div>").
+});
