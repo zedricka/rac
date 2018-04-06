a7030c8a7d58b56ddc87e28e7719269b88b3513c
zedricka <zedrick.ara@gmail.com>
2018-04-06 10:28:30 -1000

    Testing New Code

--- server.js
+++ server.js
@@ -5,29 +5,47 @@ var stripe = require('stripe')(CONFIG.SECRET_KEY);
 var path = require('path');
 var PORT = process.env.PORT || 8080;
 var app = express();
+var helpers = require('./helpers/helpers');
 
+// global variable
+var barCodeData = '';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
+app.get('/charge', function(req, res) {
+  barCodeData = req.query.data;
+  res.sendFile(path.join(__dirname + '/client/checkout.html'));
+});
+
+app.get('/sendData', function(req, res) {
+  res.send({ price: helpers._parseBarCodeData(barCodeData) });
+});
+
 app.post('/charge', function(req, res) {
+  console.log(barCodeData);
   var stripeToken = req.body.stripeToken;
-  var amount = parseInt(getPrice.join()) * 100;
 
   stripe.charges.create(
     {
       card: stripeToken,
       currency: 'usd',
-      amount: amount
+      amount: helpers._parseBarCodeData(barCodeData)
     },
     function(err, charge) {
+      console.log(charge);
       if (err) {
-        res.send(500, err);
+        res.sendStatus(500, err);
       } else {
-        res.send(204);
+        res.redirect('/confirmation.html');
       }
     }
   );
@@ -36,4 +54,3 @@ app.post('/charge', function(req, res) {
 app.listen(PORT, function() {
   console.log(`Server listening on ${PORT}`);
 });
-
