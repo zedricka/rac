d30a96d1a59db465986ae4ce1a2fd6e812c51529
Zedrick <zedrick.ara@gmail.com>
2018-04-20 09:27:44 -1000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-{
-"host" : "zedrick.cb9co1xxtizk.us-west-2.rds.amazonaws.com",
-"database" : "movies_zedrick",
-"user" : "thicc",
-"password" : "password",
-"port" : 5432,
-"table" : "movies_list"
-}
+
